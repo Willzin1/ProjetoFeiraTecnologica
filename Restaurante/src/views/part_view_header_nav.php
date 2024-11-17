cc4302706355747f3786118d3aa5aeb2e58a1210
Willzin1 <william.mendonca34@gmail.com>
2024-11-17 20:00:49 -0300

    testando subir site para azure

--- Restaurante/src/views/part_view_header_nav.php
+++ Restaurante/src/views/part_view_header_nav.php
@@ -1,20 +1,20 @@
 <!-- Onde irá ficar a NAVBAR -->  
 <header>
     <nav id="navbar">
-        <a href="./Controller/AppController.php?rota=UserCrudController.php&action=index"><i class="fa-solid fa-leaf" id="nav_logo"><br>SUSTENTA<BR>FOOD</i></a>
+        <a href="../Controller/AppController.php?rota=UserCrudController.php&action=index"><i class="fa-solid fa-leaf" id="nav_logo"><br>SUSTENTA<BR>FOOD</i></a>
 
         <ul id="nav_list">
             <li class="nav-item active">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=index">Início</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=index">Início</a>
             </li>
             <li class="nav-item">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=cardapio">Cardápio</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=cardapio">Cardápio</a>
             </li>
             <li class="nav-item">
                 <a href="#testimonials">Avaliações</a>
             </li>
             <li class="nav-item">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=perfil">Perfil</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=perfil">Perfil</a>
             </li>
         </ul>
 
@@ -30,16 +30,16 @@
     <div id="mobile_menu">
         <ul id="mobile_nav_list">
             <li class="nav-item">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=index">Início</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=index">Início</a>
             </li>
             <li class="nav-item">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=cardapio">Cardápio</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=cardapio">Cardápio</a>
             </li>
             <li class="nav-item">
                 <a href="#testimonials">Avaliações</a>
             </li>
             <li class="nav-item">
-                <a href="./Controller/AppController.php?rota=UserCrudController.php&action=perfil">Perfil</a>
+                <a href="../Controller/AppController.php?rota=UserCrudController.php&action=perfil">Perfil</a>
             </li>
         </ul>
     <!-- ESSE BOTÃO FICA NO MODO "MOBILE" -->
